2ae0db087dff21f4e103bc23cbad10dc46a0e85e
Himanshi <himanshiagarwal001@gmail.com>
2026-02-01 23:56:40 +0530

    class and object creation

--- Hello.java
+++ Hello.java
@@ -1,8 +1,18 @@
-class Hello {
-    public static void main(String args[]){
-       int n=4;
-       
+class Calculator
+{
+    public int add(int n1, int n2){
+        return n1+ n2;
+    }
+}
 
+
+
+public class Hello {
+    public static void main(String args[]){
+       Calculator obj= new Calculator();
+       int r1= obj.add(3,4);
+
+       System.out.println(r1);
        
        
     }
