9fe8890cf73dd8aa3df28d696e4c8d4486c7017c
Himanshi <himanshiagarwal001@gmail.com>
2026-02-17 22:23:24 +0530

    example of method

--- Methods.java
+++ Methods.java
@@ -1,28 +1,8 @@
-class Calculator
-{
-    public void playMusic()
-    {
-        System.out.println("Playing Music...");
-    }
-
-    public String getMeAPen(int cost)
-    {
-        if(cost>=10)
-        {
-            return "pen";
-        }else{
-            return "Nothing";
-        }
-        
-    }
-}
-
 public class Methods {
-    public static void main(String args[])
-    {
-        Calculator obj= new Calculator();
-        obj.playMusic();
-        String str= obj.getMeAPen(5);
-        System.out.println(str);
-    }
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+  public static void main(String[] args) {
+    myMethod();
+  }
 }
