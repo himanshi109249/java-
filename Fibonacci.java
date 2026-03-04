5b5280225ccc7c21e283308f648dde1e108ad380
Himanshi <himanshiagarwal001@gmail.com>
2026-03-04 20:15:53 +0530

    Fibonacci program in java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,20 +1,18 @@
 import java.util.*;
-public class Fibonacci {
-    public static void  main (String[] args){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the number of terms: ");
-            int n = sc.nextInt();
-            int a=0, b=1;
-            System.out.println(a);
-            System.out.println(b);
-            for (int i=2; i<n; i++){
-                int next= a+b;
-                System.out.println(next);
-                a=b; 
-                b= next;
-            }
-
-
+public class Fibonacci{
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n= sc.nextInt();
+        int a=0;
+        int b=1;
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+            System.out.println(sum);
+        }
+        System.out.println("Fibonacci of" + n+ " is : " + sum);
     }
-    
 }
