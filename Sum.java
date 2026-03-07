e98b55ffb87f1e606c659edac1954d424fb3a3bd
Himanshi <himanshiagarwal001@gmail.com>
2026-03-07 15:27:02 +0530

    function with return type

--- Sum.java
+++ Sum.java
@@ -1,7 +1,20 @@
 import java.util.*;
 public class Sum {
     public static void main ( String[] args){
-        sum();
+        // sum();
+        int result = sum2();
+        System.out.println(result);
+    }
+
+    static int sum2(){
+         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = sc.nextInt();
+        int sum= num1 + num2;
+        return sum;
+
     }
 
     static void sum(){
