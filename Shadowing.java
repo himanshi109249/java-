90153932b237c975c40ed6d2706b4db138edade2
Himanshi <himanshiagarwal001@gmail.com>
2026-03-07 22:48:33 +0530

    Shadowing in java

---
+++ Shadowing.java
@@ -0,0 +1,16 @@
+public class Shadowing {
+    static int x=90;
+    public static void main ( String[] args){
+        System.out.println(x); //90
+        int x; //the class variable at line 2 is shadowed by this
+        // System.out.println(x); //error : variable x is not yet been initialised
+        x= 40;
+        System.out.println(x); //40
+        fun();
+    }
+
+    static void fun(){
+        System.out.println(x);
+    }
+
+}
