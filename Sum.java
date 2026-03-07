910964e78e33d7d6429815ee09e75064e274e3e2
Himanshi <himanshiagarwal001@gmail.com>
2026-03-07 15:18:32 +0530

    sum method

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sum {
+    public static void main ( String[] args){
+        sum();
+    }
+
+    static void sum(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = sc.nextInt();
+        int sum= num1 + num2;
+        System.out.println("Sum of two numbers is : " + sum);
+
+    }
+}
