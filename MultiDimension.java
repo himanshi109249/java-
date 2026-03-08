72f093d9dbf381b6fd00aa123e8c69b4439e2608
Himanshi <himanshiagarwal001@gmail.com>
2026-03-08 16:08:13 +0530

    2D Arrays in java

---
+++ MultiDimension.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Arrays;
+public class MultiDimension {
+    public static void main (String[] args){
+        Scanner sc= new Scanner (System.in);
+        int[][] arr= new int[3][3];
+        for(int row=0; row<arr.length; row++){
+            for (int col=0; col<arr[row].length; col++){
+                arr[row][col]= sc.nextInt();
+            }
+        }
+        // for(int row=0; row<arr.length; row++){
+        //     for(int col=0; col<arr[row].length; col++){
+        //         System.out.print(arr[row][col] + "  ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //printing 2D array
+        // for(int row=0; row<arr.length; row++){
+        //     System.out.println(Arrays.toString(arr[row]));
+        // }
+
+        //inhanced for loop to print 2D array
+        for(int[] a: arr){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
