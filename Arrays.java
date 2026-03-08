1d2d008f396fe4bdc732fede4b708ceed7b81d08
Himanshi <himanshiagarwal001@gmail.com>
2026-03-08 13:50:45 +0530

    arrays in java

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main (String[] args){
+        // int[] ros;//declaration of array,  ros is getting defined int the stack
+        // ros= new int[5];//initialisation: object is being created in the memory
+        // System.out.println(ros[1]);
+
+        String[] arr= new String[4];
+        // System.out.println(arr[2]);
+
+        for(String element : arr){
+            System.out.println(element);
+        }
+    }
+}
