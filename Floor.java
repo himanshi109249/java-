747ef6622b5bb12e4108635f397bbd30f481119c
Himanshi <himanshiagarwal001@gmail.com>
2026-03-10 23:46:21 +0530

    Finding floor of a number in java

---
+++ Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+    public static void main (String[] args){
+        int[] arr= {1,2,8,10,11,12,19};
+        int target= 5;
+        int ans= floor(arr, target);
+        System.out.println(ans);
+    }
+    static int floor(int[] arr, int target){
+        int start= 0;
+        int end= arr.length-1;
+        while(start<=end){
+            int mid= start+ (end-start)/2;
+            if(arr[mid]== target){
+                return mid;
+            }else if(arr[mid]>target){
+                end= mid-1;
+            }else {
+                start= mid+1;
+            }
+            
+        }
+        return end;
+    }
+}
