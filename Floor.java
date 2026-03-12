331551d9c5e7f276bee87feed0e4073db4906c6b
Himanshi <himanshiagarwal001@gmail.com>
2026-03-12 20:18:23 +0530

    adding edge case in floor of an array

--- Floor.java
+++ Floor.java
@@ -1,11 +1,15 @@
 public class Floor {
     public static void main (String[] args){
         int[] arr= {1,2,8,10,11,12,19};
-        int target= 5;
+        int target= -2;
         int ans= floor(arr, target);
         System.out.println(ans);
     }
     static int floor(int[] arr, int target){
+        //but what if the target element is smaller than the smallest number in the array
+        if(target<arr[0]){
+            return -1;
+        }
         int start= 0;
         int end= arr.length-1;
         while(start<=end){
