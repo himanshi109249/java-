b39325d4ba20dc09182fce612e7704206dc40a92
Himanshi <himanshiagarwal001@gmail.com>
2026-03-12 20:16:48 +0530

    adding edge case in ceiling of an array

--- Ceiling.java
+++ Ceiling.java
@@ -1,11 +1,15 @@
 public class Ceiling {
     public static void main (String[] args){
         int[] arr= {1,2,8,10,11,12,19};
-        int target= 5;
+        int target= 20;
         int ans= ceiling(arr, target);
         System.out.println(ans);
     }
     static int ceiling (int[] arr, int target){
+        //but what if the target is greater than the greatest number in the array
+        if(target> arr[arr.length-1]){
+            return -1;
+        }
         int start=0;
         int end= arr.length-1;
         while(start<= end){
