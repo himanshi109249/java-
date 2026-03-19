cf8212d48921a725aee910394c0f8c42efa7398f
Himanshi <himanshiagarwal001@gmail.com>
2026-03-19 23:22:12 +0530

    java program

--- Mountain.java
+++ Mountain.java
@@ -16,14 +16,9 @@ public class Mountain {
                 end = mid;
             } else {
                 // you are in asc part of array
-                start = mid + 1; // because we know that mid+1 element > mid element
+                start = mid + 1; 
             }
         }
-        // in the end, start == end and pointing to the largest number because of the 2 checks above
-        // start and end are always trying to find max element in the above 2 checks
-        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
-        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
-        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
         return start; // or return end as both are =
     }
 } 
