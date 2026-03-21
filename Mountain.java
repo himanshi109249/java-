da480469c049a3ad030203eea83fd7a333b6530b
Himanshi <himanshiagarwal001@gmail.com>
2026-03-21 23:55:19 +0530

    array program

--- Mountain.java
+++ Mountain.java
@@ -10,12 +10,9 @@ public class Mountain {
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] > arr[mid+1]) {
-                // you are in dec part of array
-                // this may be the ans, but look at left
-                // this is why end != mid - 1
+                
                 end = mid;
             } else {
-                // you are in asc part of array
                 start = mid + 1; 
             }
         }
